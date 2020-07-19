2805f1b0dbf463b9014383ebf3e8f89e6e00ed87
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-07-19 18:54:19 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,7 +3,6 @@ require "pry"
 class CostumeStore < ActiveRecord::Migration[5.2]
   
   def change 
-    binding.pry
     create_table :costume_stores do |t|
       t.string :name 
       t.string :location
